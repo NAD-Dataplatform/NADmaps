1bdc63108f7ab112714bd7f24da9dfff0336aa03
Siebrand-HHD <svanderhoeven@hhdelfland.nl>
2026-07-05 09:50:25 +0200

    test_thema.py

--- tests/test_thema.py
+++ tests/test_thema.py
@@ -26,9 +26,9 @@ def nadmap_mock(iface_mock, selected_active_layers):
     nadmap.creator = "Gebruiker"
     nadmap.run()
     nadmap.thema_manager.creator = "Gebruiker"
-    # nadmap.thema_manager.user_thema_path = os.path.join(
-    #     nadmap.working_dir, "themas", "user_themas.json"
-    # )
+    nadmap.thema_manager.user_thema_path = os.path.join(
+        nadmap.working_dir, "themas", "user_themas.json"
+    )
     nadmap.selected_active_layers = selected_active_layers
     nadmap.dlg.saveThemaLineEdit.setText("test theme name")
     return nadmap
